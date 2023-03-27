221bb6e66c83713696a265f9ad1f3be370edd80a
Ivan Filipov <ivan@stanga.net>
2023-03-27 13:30:55 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
