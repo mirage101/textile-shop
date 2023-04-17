7a1bfa527d96a71020393a2aef5b2aeb3a32f98f
Ivan Filipov <ivan@stanga.net>
2023-04-17 13:43:31 +0300

    fix

--- server.js
+++ server.js
@@ -32,8 +32,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
-
+// app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+  const index = path.join(__dirname, "build", "./client/index.html");
+  res.sendFile(index);
+});
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
